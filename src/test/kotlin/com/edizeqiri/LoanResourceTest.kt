71bff76406f064c44dd336b85378088fc29da75c
Edi Zeqiri <89740646+edizeqiri@users.noreply.github.com>
2025-10-06 22:18:37 +0200

    feat(test): added test skeleton

--- src/test/kotlin/com/edizeqiri/LoanResourceTest.kt
+++ src/test/kotlin/com/edizeqiri/LoanResourceTest.kt
@@ -1,4 +1,68 @@
 package com.edizeqiri
 
+import io.quarkus.test.junit.QuarkusTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+@QuarkusTest
 class LoanResourceTest {
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Provide loans for userId`() {
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Sum up outstanding amount of child loans in parent loan`() {
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Find lending date range based on product date ranges`() {
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Identify if any part of a lending is overdue`() {
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Calculate next amortisation payment amount for lending`() {
+
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Identify when the next interest payment is due`() {
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+
+    )
+    fun `Show interest rate on the child loan`() {
+
+    }
+
+
 }
