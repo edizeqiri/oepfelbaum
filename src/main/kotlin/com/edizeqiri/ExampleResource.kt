d8a6322a3148205ce02f18ebd7a00f60f16e3f71
Edi Zeqiri <89740646+edizeqiri@users.noreply.github.com>
2025-10-06 21:29:47 +0200

    feat(openapi): added openapi generation of dtos

--- src/main/kotlin/com/edizeqiri/ExampleResource.kt
+++ src/main/kotlin/com/edizeqiri/ExampleResource.kt
@@ -5,10 +5,12 @@ import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
+
 @Path("/hello")
 class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = "Hello from Quarkus REST"
+
 }
---
+++ src/main/kotlin/com/edizeqiri/LoanRepository.kt
@@ -0,0 +1,6 @@
+package com.edizeqiri
+
+interface LoanRepository {
+
+
+}
